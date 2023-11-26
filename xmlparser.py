3993eeeef8f6c3843f0f09cffef4fd9636f1a3df
YasserMathComp <124908868+YasserMathComp@users.noreply.github.com>
2023-11-26 14:12:41 -0500

    Wrote XMLParser class and call from main
    First draft of writing XMLParser and calling it from main.py, needs revision when other classes are written.

--- xmlparser.py
+++ xmlparser.py
@@ -2,6 +2,27 @@ from lxml import etree
 
 
 class XmlParser:
-    def __init__(self):
-        pass
-    
+    def __init__(self, xml_file):
+        ## pass and parse xml file 
+        
+    def parse_questions(self):
+        questions = []
+
+        for question_elem in self.root.findall('question'):
+            question = {
+                'questionText': question_elem.find('questionText').text,
+                'questionImage': question_elem.find('questionImage').get('path'),
+                'answers': [],
+                'answerComments': question_elem.find('answerComments').text
+            }
+
+            for answer_elem in question_elem.findall('answer'):
+                answer = {
+                    'text': answer_elem.text,
+                    'correct': answer_elem.get('correct', 'false') == 'true'
+                }
+                question['answers'].append(answer)
+
+            questions.append(question)
+
+        return questions
