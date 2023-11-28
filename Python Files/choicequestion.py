51add8d5f19852f147450cf86db071cb57202ee5
Zion Taylor <145851093+Zion-T-2019@users.noreply.github.com>
2023-11-28 12:54:38 -0500

    Resolved conflicts with repo

--- Python Files/choicequestion.py
+++ Python Files/choicequestion.py
@@ -1,19 +1,19 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+from xmlparser import XmlParser
+from PyQt5 import QtCore, QtGui, QtWidgets
 
+
+
+
+class ChoiceQuestion:
+    def __init__(self, answer_comments):
+        self.choices = []
+        self.answer_comments = None
+            
+    def add_choices(choice, correct):
+        pass
     
-  
-=======
-from xmlparser import XmlParser
-import lxml
-
-def choicequestion():
-  
->>>>>>> 9260036a5ac8d2379cf042c932e52c5404dc241e
-=======
-from xmlparser import XmlParser
-import lxml
-
-def choicequestion():
-  
->>>>>>> 9260036a5ac8d2379cf042c932e52c5404dc241e
+    def set_answer_comments(answer_comments):
+        pass
+    
+    def display(QtLayout):
+        pass 
