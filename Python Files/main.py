9fedcf4282729e935de630484bff9ac90140dd0c
Zion Taylor <145851093+Zion-T-2019@users.noreply.github.com>
2023-11-29 18:02:23 -0500

    Added the Test ui and added it call to the main file

--- Python Files/main.py
+++ Python Files/main.py
@@ -1,7 +1,17 @@
 from xmlparser import XmlParser
 import lxml
+import sys
+from dmvdrivertestUI import UiMainWin
+from PyQt5.QtWidgets import *
 
 def main():
+    
+    app = QApplication(sys.argv)
+    main_win = QMainWindow()
+    ui = UiMainWin()
+    ui.setup_ui(main_win)
+    main_win.show()
+    sys.exit(app.exec_())
     
     # Insert the path the xml
     xml_file = "questions.xml"
