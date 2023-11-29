a2ddaae08a4d83d1f9ff24f0403c14cc554518b2
Xine-SJ <150718958+Xine-SJ@users.noreply.github.com>
2023-11-29 15:10:26 -0500

    Update choicequestion.py

--- Python Files/choicequestion.py
+++ Python Files/choicequestion.py
@@ -1,16 +1,34 @@
 from xmlparser import XmlParser
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class ChoiceQuestion:
     def __init__(self, answer_comments):
         self.choices = []
-        self.answer_comments = None
+        self.answer_comments = answer_comments
             
     def add_choices(choice, correct):
-        pass
+        # choice (str): The text of the choice.
+        # correct (bool): True if the choice is correct, False otherwise.
+        self.choices.append((choice, correct))
     
     def set_answer_comments(answer_comments):
-        pass
+        # answer_comments (str): Comments to be displayed when the user checks the answer.
+        self.answer_comments = answer_comments
     
     def display(QtLayout): 
-        pass 
+        # layout (QtLayout): The layout to which the question will be added.
+        # Creating a group for radio buttons
+        group_box = QtWidgets.QGroupBox("Choices")
+        group_layout = QtWidgets.QVBoxLayout()
+
+        # Adding radio buttons for each choice
+        for choice_text, _ in self.choices:
+            radio_button = QtWidgets.QRadioButton(choice_text)
+            group_layout.addWidget(radio_button)
+
+        group_box.setLayout(group_layout)
+
+        # Adding group box and answer comments label to the main layout
+        layout.addWidget(group_box)
+        layout.addWidget(QtWidgets.QLabel(f"Answer Comments: {self.answer_comments}"))
