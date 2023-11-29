a0ef70ce259cde363651fe4c842525dc78333a1b
Xine-SJ <150718958+Xine-SJ@users.noreply.github.com>
2023-11-29 15:12:39 -0500

    Update choicequestion.py

--- Python Files/choicequestion.py
+++ Python Files/choicequestion.py
@@ -7,16 +7,16 @@ class ChoiceQuestion:
         self.choices = []
         self.answer_comments = answer_comments
             
-    def add_choices(choice, correct):
+    def add_choice(self, choice, correct):
         # choice (str): The text of the choice.
         # correct (bool): True if the choice is correct, False otherwise.
         self.choices.append((choice, correct))
     
-    def set_answer_comments(answer_comments):
+    def set_answer_comments(self, answer_comments):
         # answer_comments (str): Comments to be displayed when the user checks the answer.
         self.answer_comments = answer_comments
     
-    def display(QtLayout): 
+    def display(self, layout): 
         # layout (QtLayout): The layout to which the question will be added.
         # Creating a group for radio buttons
         group_box = QtWidgets.QGroupBox("Choices")
