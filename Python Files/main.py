caa502ac74838973f54c5f1a0207b898895d3c1b
Zion Taylor <145851093+Zion-T-2019@users.noreply.github.com>
2023-12-02 19:26:16 -0500

    choice question

--- Python Files/main.py
+++ Python Files/main.py
@@ -5,31 +5,22 @@ from dmvdrivertestUI import UiMainWin
 from PyQt5.QtWidgets import *
 
 def main():
-    
-    app = QApplication(sys.argv)
-    main_win = QMainWindow()
-    ui = UiMainWin()
-    ui.setup_ui(main_win)
-    main_win.show()
-    sys.exit(app.exec_())
-    
     # Insert the path the xml
-    xml_file = "questions.xml"
+    xml_file = "florida_drivers_test.xml"
     parser = XmlParser(xml_file)
     questions = parser.parse_questions()
 
-    # Print parsed information
-    for i, question in enumerate(questions, 1):
-        print(f"\nQuestion {i}:")
+    # Initialize the application
+    app = QApplication(sys.argv)
+    main_win = QMainWindow()
+    ui =  UiMainWin(questions)
+    
+   
         
-        # Pass question image and text
-        print(f"Text: {question['questionText']}")
-        print(f"Image Path: {question['questionImage']}")
-        
-        print("Answers:")
-        for j, answer in enumerate(question['answers'], 1):
-            print(f"  {j}. {answer['text']} (Correct: {answer['correct']})")
-            
-        print(f"Comments: {question['answerComments']}")
+    # Set up the UI
+    ui.setup_ui(main_win)
+    main_win.show()
+    sys.exit(app.exec_())
+
 
 main()
--- Python Files/xmlparser.py
+++ Python Files/xmlparser.py
@@ -3,27 +3,28 @@ from lxml import etree
 
 class XmlParser:
     def __init__(self, xml_file):
-        ## pass and parse xml file 
-        pass
+        self.xml_file = xml_file
+        self.questions = []
     
     def parse_questions(self):
-        questions = []
-
+        #parse the xml file and return a list of questions
+        self.xml_file = etree.parse(self.xml_file)
+        self.root = self.xml_file.getroot()
         for question_elem in self.root.findall('question'):
             question = {
                 'questionText': question_elem.find('questionText').text,
-                'questionImage': question_elem.find('questionImage').get('path'),
+                'questionImage': question_elem.find('questionImage').get('path') if question_elem.find('questionImage') is not None else None,
                 'answers': [],
                 'answerComments': question_elem.find('answerComments').text
             }
 
             for answer_elem in question_elem.findall('answer'):
-                answer = {
+                answer = { 
                     'text': answer_elem.text,
                     'correct': answer_elem.get('correct', 'false') == 'true'
                 }
                 question['answers'].append(answer)
 
-            questions.append(question)
+            self.questions.append(question)
 
-        return questions
+        return self.questions
